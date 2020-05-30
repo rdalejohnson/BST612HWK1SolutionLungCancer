146b229304467b1a0d6e7e9a98d171aaf65b1c01
Robert Johnson <robertjohnson@Roberts-Mini.attlocal.net>
2020-05-30 07:38:20 -0500

    finished descriptive stats section

--- lungCancerRadomizationAnalysis.R
+++ lungCancerRadomizationAnalysis.R
@@ -3,14 +3,34 @@
 
 #install.packages("pastecs")
 library(pastecs)
+library(summarytools)
+
 
 
 
 lungCancer = read.csv("Lung_Cancer.csv")
 
+freq(lungCancer$Cell.Type)
+freq(lungCancer$Treatment)
+freq(lungCancer$Prior)
+freq(lungCancer$Age)
+freq(lungCancer$KPS)
+
 head(lungCancer)
 
 str(lungCancer)
+
+# Complete rows
+complete.cases(lungCancer)
+
+#  report position of NAs
+which(is.na(lungCancer$Cell.Type))
+which(is.na(lungCancer$Treatment))
+which(is.na(lungCancer$Prior))
+which(is.na(lungCancer$Age))
+which(is.na(lungCancer$KPS))
+
+
 
 summary(lungCancer)
 
@@ -20,14 +40,31 @@ range(lungCancer$Age)
 mean(lungCancer$Age)
 mean(lungCancer$Age, na.rm = TRUE)
 median(lungCancer$Age)
+sd(lungCancer$Age)
 
 
+#age outliers
+#https://www.r-bloggers.com/how-to-remove-outliers-in-r/
+outlierAges <- boxplot(lungCancer$Age, plot=FALSE)$out
+x<-lungCancer
+x<- x[-which(lungCancer$Age %in% outlierAges),]
+min(x$Age)
+max(x$Age)
+range(x$Age)
+mean(x$Age)
+mean(x$Age, na.rm = TRUE)
+median(x$Age)
+sd(x$Age)
+
+boxplot(lungCancer$KPS, plot=FALSE)$out
 min(lungCancer$KPS)
 max(lungCancer$KPS)
 range(lungCancer$KPS)
 mean(lungCancer$KPS)
 mean(lungCancer$KPS, na.rm = TRUE)
 median(lungCancer$KPS)
+sd(lungCancer$Age)
+
 
 by(lungCancer, lungCancer$Treatment, summary)
 stat.desc(lungCancer)
